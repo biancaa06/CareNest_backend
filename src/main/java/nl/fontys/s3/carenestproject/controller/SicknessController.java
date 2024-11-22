e0cf2a9dcbf6a912e31433e92aeace7a06e08f44
mariucaBianca <540269@student.fontys.nl>
2024-11-22 12:17:59 +0100

    sickness management functions auth

--- src/main/java/nl/fontys/s3/carenestproject/controller/SicknessController.java
+++ src/main/java/nl/fontys/s3/carenestproject/controller/SicknessController.java
@@ -1,5 +1,6 @@
 package nl.fontys.s3.carenestproject.controller;
 
+import jakarta.annotation.security.RolesAllowed;
 import lombok.AllArgsConstructor;
 import nl.fontys.s3.carenestproject.domain.classes.Sickness;
 import nl.fontys.s3.carenestproject.service.SicknessService;
@@ -30,6 +31,7 @@ public class SicknessController {
     }
 
     @PostMapping()
+    @RolesAllowed({"MANAGER"})
     public ResponseEntity<CreateSicknessResponse> createSickness(@RequestBody @Validated CreateSicknessRequest request) {
         CreateSicknessResponse response = sicknessService.createSickness(request);
         if (response == null) {
@@ -39,6 +41,7 @@ public class SicknessController {
     }
 
     @PutMapping("/id:{id}")
+    @RolesAllowed({"MANAGER"})
     public ResponseEntity<UpdateSicknessResponse> updateSickness(@PathVariable(value = "id") long id, @RequestBody @Validated UpdateSicknessRequest request) {
         try{
             sicknessService.updateSickness(id,request);
@@ -50,6 +53,7 @@ public class SicknessController {
         }
     }
     @DeleteMapping("/{id}")
+    @RolesAllowed({"MANAGER"})
     public ResponseEntity<Void> deleteSickness(@PathVariable(value = "id") long id) {
         try{
             sicknessService.deleteSicknessById(id);
